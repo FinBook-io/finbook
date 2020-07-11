0df6ccc6d5e3a299ca66264fb540a215fa499245
RaulLozanoPonce <raul.lozano.ponce@hotmail.com>
2020-07-11 14:06:25 +0100

    Artifacts have been changed

--- datahub/src/io/finbook/datahub/box/Main.java
+++ datahub/src/io/finbook/datahub/box/Main.java
@@ -11,7 +11,6 @@ public class Main {
     private static final String[] Stashes = {"Solution"};
 
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(args));
         NessGraph nessGraph = new Graph().loadStashes(Stashes).as(NessGraph.class);
         Box box = new io.intino.datahub.box.DataHubBox(args).put(nessGraph.core$()).start();
         Runtime.getRuntime().addShutdownHook(new Thread(box::stop));
